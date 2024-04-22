f1e2562483d5a577c08a2db128a32702cf00bf1e
SebGillman <sebinred1@gmail.com>
2024-04-22 12:02:51 +0100

    chore: cleanup by abstracting to functions in utils.ts

--- main.ts
+++ main.ts
@@ -1,86 +1,22 @@
-import { default as stravaImport, Strava } from "npm:strava-v3@2.2.0";
 import { Context, Hono } from "https://deno.land/x/hono@v3.3.4/mod.ts";
-import { createConfig, getAccessUrl } from "./utils.ts";
+import {
+  getAccessUrl,
+  getLoggedInAthlete,
+  getLoggedInAthleteActivities,
+  getTokenExchange,
+} from "./utils.ts";
 import { DB } from "https://deno.land/x/sqlite@v3.7.0/mod.ts";
 
 const env = new DB("./env.db");
-
-let ACCESS_TOKEN = env.query(`SELECT ACCESS_TOKEN FROM env;`)[0][0] as string;
-const CLIENT_ID = env.query(`SELECT CLIENT_ID FROM env;`)[0][0] as string;
-const REDIRECT_URI = env.query(`SELECT REDIRECT_URI FROM env;`)[0][0] as string;
-const CLIENT_SECRET = env.query(
-  `SELECT CLIENT_SECRET FROM env;`,
-)[0][0] as string;
-let REFRESH_TOKEN = env.query(`SELECT REFRESH_TOKEN FROM env;`)[0][0] as string;
-
-const strava = stravaImport as unknown as Strava;
-
-let config = createConfig(
-  ACCESS_TOKEN,
-  CLIENT_ID,
-  CLIENT_SECRET,
-  REDIRECT_URI,
-);
-
-strava.config(config);
 
 const app = new Hono();
 
 app.get(
   "/auth/login",
   async (c: Context) => {
-    try {
-      // const d = new Date();
-      // const seconds = Math.round(d.getTime() / 1000);
-      // const accessTokenExpirySeconds = Number(
-      //   env.query("SELECT ACCESS_TOKEN_EXPIRES_AT FROM env;")[0][0],
-      // );
-      // if (
-      //   isNaN(accessTokenExpirySeconds) ||
-      //   accessTokenExpirySeconds - seconds < 60 * 60
-      // ) throw new Error();
-      const accessUrl = await getAccessUrl(strava, config);
-      return c.redirect(accessUrl);
-    } catch {
-      console.log("CORRECT");
-      const refreshResponse = await fetch(
-        "https://www.strava.com/api/v3/oauth/token",
-        {
-          method: "POST",
-          headers: new Headers({
-            "client_id": CLIENT_ID,
-            "client_secret": CLIENT_SECRET,
-            "grant_type": "refresh_token",
-            "refresh_token": REFRESH_TOKEN,
-          }),
-        },
-      );
-      const refreshResponseJSON = await refreshResponse.json();
-      console.log(refreshResponseJSON);
-      return c.text("");
-      const {
-        expires_at: expiresAt,
-      } = refreshResponseJSON;
-
-      ACCESS_TOKEN = refreshResponseJSON.access_token;
-      REFRESH_TOKEN = refreshResponseJSON.refresh_token;
-      env.execute(`
-      UPDATE env
-      SET ACCESS_TOKEN = '${ACCESS_TOKEN}',
-      REFRESH_TOKEN = '${REFRESH_TOKEN}',
-      ACCESS_TOKEN_EXPIRES_AT = '${expiresAt as string}'
-      WHERE CLIENT_ID = '${CLIENT_ID}';
-      `);
-
-      const config = createConfig(
-        ACCESS_TOKEN,
-        CLIENT_ID,
-        CLIENT_SECRET,
-        REDIRECT_URI,
-      );
-      const accessUrl = await getAccessUrl(strava, config);
-      return c.redirect(accessUrl);
-    }
+    const accessUrl = await getAccessUrl(env);
+    console.log("GO TO", accessUrl);
+    return c.redirect(accessUrl);
   },
 );
 
@@ -88,85 +24,16 @@ app.get(
   "/auth/access-code",
   async (c: Context) => {
     try {
-      const reqUrl = new URL(c.req.url);
-      const reqSearchParams = reqUrl.searchParams;
-      console.log(reqSearchParams);
-      if (!reqSearchParams.has("code")) {
-        return c.redirect("/auth/login");
-      }
-      const accessCode = reqSearchParams.get("code");
-      if (!accessCode) throw new Error("Invalid access code");
-      env.execute(`UPDATE env
-      SET ACCESS_CODE = '${accessCode}'
-      WHERE CLIENT_ID = '${CLIENT_ID}';
-      `);
+      await getTokenExchange(c, env);
 
-      const tokenExchange = await fetch(
-        "https://www.strava.com/api/v3/oauth/token",
-        {
-          method: "POST",
-          body: new URLSearchParams({
-            "client_id": CLIENT_ID,
-            "client_secret": CLIENT_SECRET,
-            "code": accessCode,
-            "grant_type": "authorization_code", //todo
-          }),
-        },
+      const me = await getLoggedInAthlete(env);
+
+      const myActivities = await getLoggedInAthleteActivities(env);
+
+      return c.text(
+        JSON.stringify(await me.json()) + "\n\n" +
+          JSON.stringify(await myActivities.json()),
       );
-      // console.log(tokenExchange);
-      if (tokenExchange.ok) {
-        const tokenData = await tokenExchange.json();
-
-        const tokenExpiresAt = tokenData.expires_at;
-        // const tokenExpiresIn = tokenData.expires_in;
-        REFRESH_TOKEN = tokenData.refresh_token;
-        ACCESS_TOKEN = tokenData.access_token;
-
-        env.execute(`
-      UPDATE env
-      SET ACCESS_TOKEN = '${ACCESS_TOKEN}',
-      REFRESH_TOKEN = '${REFRESH_TOKEN}',
-      ACCESS_TOKEN_EXPIRES_AT = '${tokenExpiresAt as string}'
-      WHERE CLIENT_ID = '${CLIENT_ID}';
-      `);
-        config = createConfig(
-          ACCESS_TOKEN,
-          CLIENT_ID,
-          CLIENT_SECRET,
-          REDIRECT_URI,
-        );
-        // strava.config(config);
-
-        const me = await fetch(
-          "https://www.strava.com/api/v3/athlete",
-          {
-            method: "GET",
-            headers: new Headers({
-              "Authorization": `Bearer ${ACCESS_TOKEN}`,
-              "Accept": "application/json",
-            }),
-          },
-        );
-
-        const myActivities = await fetch(
-          "https://www.strava.com/api/v3/athlete/activities",
-          {
-            method: "GET",
-            headers: new Headers({
-              "Authorization": `Bearer ${ACCESS_TOKEN}`,
-              "Accept": "application/json",
-            }),
-          },
-        );
-
-        return c.text(
-          "HELLO" + "\n\n" +
-            JSON.stringify(await me.json()) + "\n\n" +
-            JSON.stringify(await myActivities.json()),
-        );
-      } else {
-        return c.text("ERROR OCCURED");
-      }
     } catch (error) {
       return c.text(error);
     }
--- utils.ts
+++ utils.ts
@@ -1,5 +1,5 @@
-import { Strava } from "npm:strava-v3@2.2.0";
-import { Config } from "./types.ts";
+import { Context } from "https://deno.land/x/hono@v3.3.4/mod.ts";
+import { DB } from "https://deno.land/x/sqlite@v3.7.0/mod.ts";
 
 export function createConfig(
   ACCESS_TOKEN: string | undefined,
@@ -30,18 +30,116 @@ export function createConfig(
 }
 
 export async function getAccessUrl(
-  strava: Strava,
-  config: Config,
+  env: DB,
 ): Promise<string> {
   try {
-    const accessURL = await strava.oauth.getRequestAccessURL({
-      "client_id": config.client_id,
-      "redirect_uri": config.redirect_uri,
+    const CLIENT_ID = await getEnvVar(env, "CLIENT_ID");
+    const REDIRECT_URI = await getEnvVar(env, "REDIRECT_URI");
+
+    const queryParams = {
+      "client_id": CLIENT_ID,
+      "redirect_uri": REDIRECT_URI,
       "response_type": "code",
       "scope": "activity:read_all,profile:read_all",
+    };
+
+    const url = new URL("https://www.strava.com/oauth/authorize");
+
+    Object.entries(queryParams).forEach(([key, value]) => {
+      url.searchParams.append(key, value);
     });
-    return accessURL;
+
+    const urlString = url.toString();
+
+    return urlString;
   } catch (error) {
     throw new Error(error);
   }
+}
+
+export async function getTokenExchange(
+  c: Context,
+  env: DB,
+) {
+  const CLIENT_ID = getEnvVar(env, "CLIENT_ID");
+  const CLIENT_SECRET = getEnvVar(env, "CLIENT_SECRET");
+  let REFRESH_TOKEN = getEnvVar(env, "REFRESH_TOKEN");
+  let ACCESS_TOKEN = getEnvVar(env, "ACCESS_TOKEN");
+
+  const reqUrl = new URL(c.req.url);
+  const reqSearchParams = reqUrl.searchParams;
+  console.log(reqSearchParams);
+  if (!reqSearchParams.has("code")) {
+    return c.redirect("/auth/login");
+  }
+  const accessCode = reqSearchParams.get("code");
+  if (!accessCode) throw new Error("Missing access code");
+  env.execute(`UPDATE env
+      SET ACCESS_CODE = '${accessCode}'
+      WHERE CLIENT_ID = '${CLIENT_ID}';
+      `);
+
+  const tokenExchange = await fetch(
+    "https://www.strava.com/api/v3/oauth/token",
+    {
+      method: "POST",
+      body: new URLSearchParams({
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        "code": accessCode,
+        "grant_type": "authorization_code", //todo
+      }),
+    },
+  );
+  // console.log(tokenExchange);
+  if (tokenExchange.ok) {
+    const tokenData = await tokenExchange.json();
+
+    const tokenExpiresAt = tokenData.expires_at;
+    // const tokenExpiresIn = tokenData.expires_in;
+    REFRESH_TOKEN = tokenData.refresh_token;
+    ACCESS_TOKEN = tokenData.access_token;
+
+    env.execute(`
+      UPDATE env
+      SET ACCESS_TOKEN = '${ACCESS_TOKEN}',
+      REFRESH_TOKEN = '${REFRESH_TOKEN}',
+      ACCESS_TOKEN_EXPIRES_AT = '${tokenExpiresAt as string}'
+      WHERE CLIENT_ID = '${CLIENT_ID}';
+      `);
+  } else {
+    throw new Error("Token Exchange failed.");
+  }
+}
+
+export function getEnvVar(env: DB, column: string) {
+  return env.query(`SELECT ${column} FROM env;`)[0][0] as string;
+}
+
+export async function getLoggedInAthlete(env: DB) {
+  const ACCESS_TOKEN = getEnvVar(env, "ACCESS_TOKEN");
+  return await fetch(
+    "https://www.strava.com/api/v3/athlete",
+    {
+      method: "GET",
+      headers: new Headers({
+        "Authorization": `Bearer ${ACCESS_TOKEN}`,
+        "Accept": "application/json",
+      }),
+    },
+  );
+}
+
+export async function getLoggedInAthleteActivities(env: DB) {
+  const ACCESS_TOKEN = getEnvVar(env, "ACCESS_TOKEN");
+  return await fetch(
+    "https://www.strava.com/api/v3/athlete/activities",
+    {
+      method: "GET",
+      headers: new Headers({
+        "Authorization": `Bearer ${ACCESS_TOKEN}`,
+        "Accept": "application/json",
+      }),
+    },
+  );
 }
