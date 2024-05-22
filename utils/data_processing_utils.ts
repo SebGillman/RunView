9d447f5877a48d702c8290916ba2373a0f41283a
SebGillman <sebinred1@gmail.com>
2024-05-22 14:55:33 +0100

    refactor: getWeeklyDistance now gives empty weeks within start-end range and takes filter year

--- utils/data_processing_utils.ts
+++ utils/data_processing_utils.ts
@@ -9,42 +9,60 @@ function getWeekStart(originalDate: Date): string {
   return `${date.getMonth() + 1}-${date.getDate()}`;
 }
 
-export async function getWeeklyDistance(env: Client) {
+export async function getWeeklyDistance(env: Client, filterYear?: string) {
   const data: Activity[] = await getLoggedInAthleteActivities(env);
   const grouped: { [key: string]: number } = {};
-  data.reverse().forEach((item) => {
-    const date = new Date(item.start_date);
-    const week = `${date.getFullYear()}-${getWeekStart(date)}`;
-    if (!(week in grouped)) {
-      grouped[week] = 0;
-    }
-    grouped[week] = grouped[week] + item.distance / 1000;
-  });
-  return grouped;
-}
-
-export async function getCumulativeYearDistance(env: Client, year: string) {
-  const data: Activity[] = await getLoggedInAthleteActivities(env);
-  const grouped: { [key: string]: number } = {};
-  let prevWeek: string;
   data
     .filter((activity) => {
-      return activity.start_date.startsWith(year);
+      return filterYear ? activity.start_date.startsWith(filterYear) : true;
     })
     .reverse()
     .forEach((item) => {
       const date = new Date(item.start_date);
       const week = `${date.getFullYear()}-${getWeekStart(date)}`;
       if (!(week in grouped)) {
-        prevWeek ? (grouped[week] = grouped[prevWeek]) : (grouped[week] = 0);
-        prevWeek = week;
+        grouped[week] = 0;
       }
       grouped[week] = grouped[week] + item.distance / 1000;
     });
-  return grouped;
+
+  const keys = Object.keys(grouped);
+
+  const startDate = new Date(keys[0]);
+  const endDate = new Date(keys[keys.length - 1]);
+
+  const interpolatedGrouped: { [key: string]: number } = {};
+
+  for (
+    const date = startDate;
+    date < endDate;
+    date.setDate(date.getDate() + 7)
+  ) {
+    const dateString = `${date.getFullYear()}-${
+      date.getMonth() + 1
+    }-${date.getDate()}`;
+
+    interpolatedGrouped[dateString] =
+      keys.indexOf(dateString) !== -1 ? grouped[dateString] : 0;
+  }
+
+  return interpolatedGrouped;
+}
+
+export async function getCumulativeWeeklyYearDistance(
+  env: Client,
+  year: string
+) {
+  const data: { [key: string]: number } = await getWeeklyDistance(env, year);
+  let prev = 0;
+  for (const key of Object.keys(data)) {
+    data[key] += prev;
+    prev = data[key];
+  }
+  return data;
 }
 
 export async function getCurrentCumulativeYearDistance(env: Client) {
-  const res = await getCumulativeYearDistance(env, "2024");
+  const res = await getCumulativeWeeklyYearDistance(env, "2024");
   return res;
 }
